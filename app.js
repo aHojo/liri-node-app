796cb0b116c3bd52469b0ce4fb340731adf8a850
amh5366 <amh5366@upenn.edu>
2018-07-15 18:54:14 -0400

    Added modules, git ignore, require statements, spotify api keys

---
+++ app.js
@@ -0,0 +1,11 @@
+//Docs https://www.npmjs.com/package/dotenv
+require('dotenv').config();
+
+//Docs https://www.npmjs.com/package/request
+const request = require("request");
+
+//Docs https://www.npmjs.com/package/twitter
+const Twitter = require('twitter');
+
+//Docs https://www.npmjs.com/package/node-spotify-api https://developer.spotify.com/dashboard/
+const Spotify = require('node-spotify-api');
