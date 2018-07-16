c494b84dcc1a07a3a53c50e0d1a7dc7c1ac4505a
amh5366 <amh5366@upenn.edu>
2018-07-15 23:13:44 -0400

    added ESLINT stuff and twitter api call

--- app.js
+++ app.js
@@ -22,43 +22,66 @@ let command = process.argv[2];
 * `do-what-it-says
 */
 
-switch(command) {
+/** Displays tweets when called */
+let displayTweets = function() {
+
+    let client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    });
+
+    client.get("statuses/user_timeline", {screen_name: "KidLiri", count: "20"}, function(err, tweets, response) {
+        if (err) {
+            return console.log(err);
+        }
+        for (let i = 0; i < tweets.length; i++) {
+            process.stdout.write(`${tweets[i].user.name} Tweeted: ${tweets[i].created_at} on ${tweets[i].text}\n`);
+        }
+
+        return 0;
+    });
+}
+
+switch (command) {
     case 'help':
-    //TODO Display a list of commands and their uses.
-    break;
+        //TODO Display a list of commands and their uses.
+        break;
     case 'my-tweets':
-    //TODO This will show your last 20 tweets and when they were created at in your terminal/bash window.
-    break;
+        //TODO This will show your last 20 tweets and when they were created at in your terminal/bash window.
+        displayTweets();
+        break;
     case "spotify-this-song":
-    /*TODO This will show the following information about the song in your terminal/bash window
-     * Artist(s)
-     * The song's name
-     * A preview link of the song from Spotify
-     * The album that the song is from
-     * If no song is provided then your program will default to "The Sign" by Ace of Base
-     */
-    break;
+        /*TODO This will show the following information about the song in your terminal/bash window
+         * Artist(s)
+         * The song's name
+         * A preview link of the song from Spotify
+         * The album that the song is from
+         * If no song is provided then your program will default to "The Sign" by Ace of Base
+         */
+        break;
     case 'movie-this':
-    /* TODO
-    * This will output the following information to your terminal/bash window:
-    * Title of the movie.
-    * Year the movie came out.
-    * IMDB Rating of the movie.
-    * Rotten Tomatoes Rating of the movie.
-    * Country where the movie was produced.
-    * Language of the movie.
-    * Plot of the movie.
-    * Actors in the movie.
-    * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.' 
-    */
-    break;
+        /* TODO
+        * This will output the following information to your terminal/bash window:
+        * Title of the movie.
+        * Year the movie came out.
+        * IMDB Rating of the movie.
+        * Rotten Tomatoes Rating of the movie.
+        * Country where the movie was produced.
+        * Language of the movie.
+        * Plot of the movie.
+        * Actors in the movie.
+        * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+        */
+        break;
     case 'do-what-it-says':
-    /* TODO
-    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-    * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-    * Feel free to change the text in that document to test out the feature for other commands.
-    */
-    break;
+        /* TODO
+        * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+        * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+        * Feel free to change the text in that document to test out the feature for other commands.
+        */
+        break;
     default:
     //TODO Display list of commands if not in this list
 }
@@ -66,7 +89,7 @@ switch(command) {
 /*
 ### BONUS
 * In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
-* Make sure you append each command you run to the `log.txt` file. 
+* Make sure you append each command you run to the `log.txt` file.
 * Do not overwrite your file each time you run a command.
 ### Reminder: Submission on BCS
 * Please submit the link to the Github Repository!
